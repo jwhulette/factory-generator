436f7c03eb0cb0c8c02b0e04005d9a9880bd7e6f
jwhulette <jwhulette@users.noreply.github.com>
2021-06-06 18:59:47 +0000

    Fix styling

--- tests/Unit/__snapshots__/files/FactoryGeneratorCommandTest__testCreateFactoryOptionSetNullDefault__1.php
+++ tests/Unit/__snapshots__/files/FactoryGeneratorCommandTest__testCreateFactoryOptionSetNullDefault__1.php
@@ -2,8 +2,8 @@
 
 namespace Database\Factories;
 
-use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 use Jwhulette\FactoryGenerator\Tests\Models\Generator;
 
 class GeneratorFactory extends Factory
