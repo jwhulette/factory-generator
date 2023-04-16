4f99725f93a4e0f634f9c3a4b0ffd5074d43e8b0
Wes Hulette <jwhulette@gmail.com>
2023-04-16 13:55:58 -0400

    Updated snapshots

--- tests/Unit/__snapshots__/files/FactoryGeneratorCommandTest__it_can_create_a_factory_with_windows_path__1.php
+++ tests/Unit/__snapshots__/files/FactoryGeneratorCommandTest__it_can_create_a_factory_with_windows_path__1.php
@@ -7,7 +7,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Jwhulette\FactoryGenerator\Tests\Models\Generator;
 
-class FactoryGeneratorCommandTest__it_can_create_a_factory_with_windows_path__1 extends Factory
+class GeneratorFactory extends Factory
 {
     protected $model = Generator::class;
 
